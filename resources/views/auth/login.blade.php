5488845580bdf5c061f8c9386537a97c8b3f3adf
Shanda764 <shandadwijri7@gmail.com>
2025-02-18 15:36:43 +0700

    delete regist in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,24 +37,16 @@
                             <div class="col-6">
                                 <button class="btn btn-primary px-4" type="submit">{{ __('Login') }}</button>
                             </div>
-                            @if (Route::has('password.request'))
+                            {{-- @if (Route::has('password.request'))
                                 <div class="col-6 text-end">
                                     <a href="{{ route('password.request') }}" class="btn btn-link px-0"
                                        type="button">{{ __('Forgot Your Password?') }}</a>
                                 </div>
-                            @endif
+                            @endif --}}
                         </div>
                     </form>
                 </div>
             </div>
-            <div class="card col-md-5 text-white bg-primary py-5">
-                <div class="card-body text-center">
-                    <div>
-                        <h2>{{ __('Sign up') }}</h2>
-                        <a href="{{ route('register') }}"
-                           class="btn btn-lg btn-outline-light mt-3">{{ __('Register') }}</a>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
